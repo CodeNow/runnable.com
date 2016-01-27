0b0451b67beaab6e49eae2e03af5834fb14205b7
runnabro <tli415@gmail.com>
2016-01-26 17:09:46 -0800

    add .gitignore, gulp, package.json

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp');
+var debug = require('gulp-debug'); // debug
+var newer = require('gulp-newer'); // file changes
+var fileinclude = require('gulp-file-include'); // html
+var sass = require('gulp-sass'); // sass
+var autoprefixer = require('gulp-autoprefixer'); // autoprefixer
+var imagemin = require('gulp-imagemin'); // image optimizer
+
+// file locations
+var src = 'src/';
+var htmlDir = src + 'html/**/*.html';
+var htmlSrc = src + 'html/index.html';
+var sassDir = src +'styles/**/*.scss';
+var sassSrc = src + 'styles/index.scss';
+var imgDir = src + 'images/**/*.*';
+
+var dist = './dist/';
+var htmlDist = dist;
+var sassDist = dist + 'styles/';
+var imgDist = dist + 'images/';
+
+// html files
+gulp.task('fileinclude', function() {
+  gulp.src([htmlSrc])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .on('error', function(err){
+      console.log(err.message);
+    })
+    .pipe(gulp.dest(htmlDist));
+});
+
+// sass
+gulp.task('sass', function() {
+  gulp.src(sassSrc)
+    .pipe(sass({
+        errLogToConsole: true
+    }))
+    .on('error', function(err){
+      console.log(err.message);
+    })
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest(sassDist));
+});
+
+// images
+gulp.task('imagemin', function () {
+  return gulp.src(imgDir)
+    .pipe(newer(imgDist))
+    .pipe(imagemin({
+        svgoPlugins: [{removeViewBox: false}]
+    }))
+    .pipe(debug({
+      title: 'imagemin'
+    }))
+    .pipe(gulp.dest(imgDist));
+});
+
+// watch task
+gulp.task('default', ['fileinclude', 'sass'], function() {
+    gulp.watch(htmlDir, ['fileinclude']);
+    gulp.watch(sassDir, ['sass']);
+    gulp.watch(imgDir, ['imagemin']);
+});
