c029c258459b3f3aae27745c5d4c4b58b1581d1c
runnabro <tli415@gmail.com>
2016-02-17 20:05:16 -0800

    validate only dirty inputs

--- src/js/index.js
+++ src/js/index.js
@@ -25,17 +25,27 @@ app.controller('MainCtrl', function ($scope, $window, $http) {
       }
       $window.location = '/' + org;
     });
+
+  // confirming form submit
+  // var articleSignUp = document.getElementsByClassName('article-sign-up')[0];
+  var formSignUp = document.getElementsByClassName('form-sign-up')[0];
+  var inputEmail = document.getElementById('mce-EMAIL');
+  var inputOrg = document.getElementById('mce-GH_ORG');
+
+  function makeDirty(e){
+    e.target.classList.remove('pristine');
+  }
+
+  function formSubmit(e){
+    if (formSignUp.checkValidity()) {
+      // articleSignUp.classList.add('submitted');
+    } else {
+      e.preventDefault();
+    }
+  }
+
+  window.onload = function(){
+    formSignUp.addEventListener('change', makeDirty);
+    formSignUp.addEventListener('submit', formSubmit);
+  };
 });
-
-// confirming form submit
-// var articleSignUp = document.getElementsByClassName('article-sign-up')[0];
-// var formSignUp = document.getElementsByClassName('form-sign-up')[0];
-
-// function formSubmit(e){
-//   e.preventDefault();
-//   articleSignUp.classList.add('submitted');
-// }
-
-// window.onload = function(){
-//   formSignUp.addEventListener('submit', formSubmit);
-// };
