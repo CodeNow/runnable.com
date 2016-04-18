bc819aacb4b254acc3c3b4ec1fd48df0a9ecee47
runnabro <tli415@gmail.com>
2016-04-18 12:34:15 -0700

    Update 2016-04-18T19:34:13.059Z

--- js/signup.js
+++ js/signup.js
@@ -40,7 +40,7 @@ function shakeForm(e) {
   });
 }
 
-function makeDirty(e) {
+function makeDirty(e){
   e.target.classList.remove('pristine', 'invalid');
 }
 
@@ -52,48 +52,36 @@ function formInvalid(e) {
 function toggleEditing(form, state) {
   var i;
   var theseInputs = form.getElementsByTagName('input');
-  var theseTextareas = form.getElementsByTagName('textarea')[0];
   var submitButton = form.getElementsByTagName('button')[0];
   var spinner = document.getElementsByClassName('spinner-wrapper');
 
   if (state === 'disable') {
-    if (theseInputs) {
-      for (i = 0; i < theseInputs.length; i++) {
-        theseInputs[i].disabled = true;
-      }
-    }
-    if (theseTextareas) {
-      theseTextareas.disabled = true;
+    for (i = 0; i < theseInputs.length; i++) {
+      theseInputs[i].disabled = true;
     }
     submitButton.disabled = true;
     submitButton.innerHTML += '<div class="spinner-wrapper spinner-md"><svg viewbox="0 0 16 16" class="spinner"><circle cx="8" cy="8" r="7" stroke-linecap="round" class="path"></circle></svg></div>';
   }
 
   if (state === 'enable') {
-    if (theseInputs) {
-      for (i = 0; i < theseInputs.length; i++) {
-        theseInputs[i].disabled = false;
-      }
-    }
-    if (theseTextareas) {
-      theseTextareas.disabled = false;
+    for (i = 0; i < theseInputs.length; i++) {
+      theseInputs[i].disabled = false;
     }
     submitButton.disabled = false;
     spinner[0].parentElement.removeChild(spinner[0]);
   }
 }
 
-function submitSignUp(e) {
+function formSubmit(e){
   var form = e.target;
   e.preventDefault();
+  toggleEditing(form, 'disable'); // disables inputs
 
   if (form.checkValidity()) {
     var scm = document.getElementsByName('scm');
     var scmName = '';
     var formData;
     var xhr = new XMLHttpRequest();
-
-    toggleEditing(form, 'disable'); // disables inputs
 
     // jsonify form data
     for(var i = 0; i < scm.length; i++) {
@@ -118,7 +106,7 @@ function submitSignUp(e) {
     xhr.onreadystatechange = function() {
       if ( xhr.readyState === 4 && xhr.status === 0) {
         shakeForm(e);
-        activeCampaignValidation('An unknown error occured. Please send us an email at <a class="link" href="mailto:support@runnable.com">support@runnable.com</a> for assistance.', form);
+        activeCampaignValidation('An unknown error occured. Please send us an email at support@runnable.com for assistance.');
       }
 
       toggleEditing(form, 'enable'); // re-enables form
@@ -128,8 +116,6 @@ function submitSignUp(e) {
       var response = JSON.parse(xhr.responseText);
       var resultCode = response.result_code;
       var resultMessage = response.result_message;
-      var subscriberId = response.subscriber_id;
-      var subscriberEmail = response.subscriberEmail;
 
       // result_codes:
       // -1 = error from sundip
@@ -138,12 +124,12 @@ function submitSignUp(e) {
 
       if (resultCode === -1 || resultCode === 0) {
         shakeForm(e);
-        activeCampaignValidation(resultMessage, form);
+        activeCampaignValidation(resultMessage);
       }
 
       if (resultCode === 1) {
         document.getElementsByClassName('article-sign-up')[0].classList.add('out');
-        document.getElementsByClassName('article-questionnaire')[0].classList.add('in');
+        document.getElementsByClassName('article-confirm')[0].classList.add('in');
       }
 
       toggleEditing(form, 'enable'); // re-enables form
@@ -160,71 +146,9 @@ function submitSignUp(e) {
   }
 }
 
-function submitQuestionnaire(e) {
-  var form = e.target;
-  e.preventDefault();
-
-  if (form.checkValidity()) {
-    var formData;
-    var xhr = new XMLHttpRequest();
-
-    // WIP: need to get these from submitSignUp()
-    var subscriberId;
-    var subscriberEmail;
-
-    toggleEditing(form, 'disable'); // disables inputs
-
-    formData = {
-      subscriberId: subscriberId,
-      subscriberEmail: subscriberEmail,
-      questionnaire: form[0].value
-    };
-
-    formData = JSON.stringify(formData); // convert to JSON
-
-    // send form
-    xhr.open('POST', 'https://marketing-88rbj4hy.cloudapp.net/submit2');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(formData);
-
-    xhr.onreadystatechange = function() {
-      if ( xhr.readyState === 4 && xhr.status === 0) {
-        shakeForm(e);
-        activeCampaignValidation('An unknown error occured. Please send us an email at support@runnable.com for assistance.', form);
-      }
-
-      toggleEditing(form, 'enable'); // re-enables form
-    };
-
-    xhr.onload = function() {
-      var response = JSON.parse(xhr.responseText);
-      var resultCode = response.result_code;
-      var resultMessage = response.result_message;
-
-      // result_codes:
-      // -1 = error from sundip
-      // 0 = error from active campaign
-      // 1 = success from active campaign
-
-      if (resultCode === -1 || resultCode === 0) {
-        shakeForm(e);
-        activeCampaignValidation(resultMessage, form);
-      }
-
-      if (resultCode === 1) {
-        document.getElementsByClassName('article-questionnaire')[0].classList.add('out');
-        document.getElementsByClassName('article-confirm')[0].classList.add('in');
-      }
-
-      toggleEditing(form, 'enable'); // re-enables form
-    };
-  }
-}
-
-function activeCampaignValidation(resultMessage, form) {
-  var thisArticle = form.parentNode;
-  var thisErrorWell = thisArticle.getElementsByClassName('well-error')[0];
-  var thisErrorText = thisArticle.getElementsByClassName('well-text')[0];
+function activeCampaignValidation(resultMessage) {
+  var errorWell = document.getElementsByClassName('well-error')[0];
+  var errorText = document.getElementsByClassName('well-text')[0];
   var firstSentence = resultMessage.substr(0, resultMessage.indexOf('.'));
 
   // change error text from active campaign
@@ -237,8 +161,8 @@ function activeCampaignValidation(resultMessage, form) {
       break;
   }
 
-  thisErrorText.innerHTML = resultMessage;
-  thisErrorWell.setAttribute('style', 'display: flex !important');
+  errorText.innerHTML = resultMessage;
+  errorWell.setAttribute('style', 'display: flex !important');
 }
 
 // check scrolling
@@ -254,8 +178,7 @@ function checkScroll() {
 // events
 window.addEventListener('load', function(){
   var i;
-  var signUpForm = document.getElementsByClassName('form-sign-up')[0];
-  var questionnaireForm = document.getElementsByClassName('form-questionnaire')[0];
+  var modalForms = document.getElementsByClassName('modal-backdrop');
   var imgFlip = document.getElementsByClassName('img-rounded');
   var theseInputs;
 
@@ -263,18 +186,18 @@ window.addEventListener('load', function(){
   window.addEventListener('hashchange', checkScroll);
 
   // modal forms
-  if (signUpForm) {
-    signUpForm.addEventListener('change', makeDirty);
-    signUpForm.addEventListener('submit', submitSignUp);
-    questionnaireForm.addEventListener('change', makeDirty);
-    questionnaireForm.addEventListener('submit', submitQuestionnaire);
+  if (modalForms) {
+    for (i = 0; i < modalForms.length; i++) {
+      modalForms[i].addEventListener('change', makeDirty);
+      modalForms[i].addEventListener('submit', formSubmit);
 
-    theseInputs = signUpForm.getElementsByTagName('input');
-    for (i = 0; i < theseInputs.length; i++) {
-      theseInputs[i].addEventListener('invalid', formInvalid);
+      theseInputs = modalForms[i].getElementsByTagName('input');
+      for (i = 0; i < theseInputs.length; i++) {
+        theseInputs[i].addEventListener('invalid', formInvalid);
 
-      if (theseInputs[i].classList.contains('input-radio')) {
-        theseInputs[i].addEventListener('change', updateLabel);
+        if (theseInputs[i].classList.contains('input-radio')) {
+          theseInputs[i].addEventListener('change', updateLabel);
+        }
       }
     }
   }
