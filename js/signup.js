2b39ea96bb141d9536d8af95cb95a47f78bb197f
runnabro <tli415@gmail.com>
2016-06-29 18:48:21 -0700

    Update 2016-06-30T01:48:18.621Z

--- js/signup.js
+++ js/signup.js
@@ -1,71 +1,41 @@
-// modals
-function openModal(dragging) {
-  event.preventDefault();
+// sign up form
+function openSignUp(dragging, signUpModal) {
   if (!dragging) {
-    var openModal = document.getElementsByClassName('modal-backdrop in')[0];
-    var modalName = event.target.getAttribute('data-target').substring(1);
-    var modal = document.getElementById(modalName);
-    var closeTrigger = modal.getElementsByClassName('js-modal-close')[0];
-
-    // close open modal
-    if (openModal) {
-      openModal.classList.remove('in');
-    }
+    var closeTrigger = document.getElementsByClassName('js-modal-close')[0];
     // show modal
-    modal.classList.add('in');
+    signUpModal.classList.add('in');
     // stop scrolling
     document.body.classList.add('modal-open');
     // triggers for close button
-    closeTrigger.addEventListener('click', closeModal);
-    closeTrigger.addEventListener('touchend', closeModal);
+    closeTrigger.addEventListener('click', closeSignUp, false);
+    closeTrigger.addEventListener('touchend', closeSignUp, false);
     // trigger for esc key
     document.addEventListener('keydown', escModal);
-    // bind sign up events
-    if (modalName === 'sign-up') {
-      setupSignUp();
-    }
   }
 }
 
-function escModal() {
-  if (event.keyCode == 27) {
-    closeModal();
-  }
-}
+function closeSignUp(e) {
+  e.preventDefault();
 
-function closeModal() {
-  var modal = document.getElementsByClassName('modal-backdrop in')[0];
-  var closeTrigger = modal.getElementsByClassName('js-modal-close')[0];
-
-  event.preventDefault();
+  var closeTrigger = document.getElementsByClassName('js-modal-close')[0];
   // hide modal
-  modal.classList.remove('in');
+  document.getElementById('sign-up').classList.remove('in');
   // resume scrolling
   document.body.classList.remove('modal-open');
   // remove triggers
-  closeTrigger.removeEventListener('click', closeModal);
-  closeTrigger.removeEventListener('touchend', closeModal);
+  closeTrigger.removeEventListener('click', closeSignUp, false);
+  closeTrigger.removeEventListener('touchend', closeSignUp, false);
   document.removeEventListener('keydown', escModal);
 }
 
-// sign up
-function setupSignUp() {
-  var signUpForm = document.getElementsByClassName('form-sign-up')[0];
-  var theseInputs = signUpForm.getElementsByTagName('input');
-
-  signUpForm.addEventListener('change', makeDirty);
-  signUpForm.addEventListener('submit', submitSignUp);
-  for (i = 0; i < theseInputs.length; i++) {
-    theseInputs[i].addEventListener('invalid', formInvalid);
-    if (theseInputs[i].classList.contains('input-radio')) {
-      theseInputs[i].addEventListener('change', updateLabel);
-    }
+function escModal(e) {
+  if (e.keyCode == 27) {
+    closeSignUp(e);
   }
 }
 
 function updateLabel(e) {
   var label = document.getElementsByClassName('label-text')[0];
-
   switch (e.target.getAttribute('value')) {
     case 'GitHub':
       label.innerHTML = 'GitHub Organization';
@@ -80,7 +50,6 @@ function markInvalid(e) {
   var thisTarget = e.target;
   var theseInputs;
   var i;
-
   if (thisTarget.tagName == 'INPUT') {
     // for invalid event
     theseInputs = thisTarget.classList.add('invalid');
@@ -97,7 +66,6 @@ function markInvalid(e) {
 
 function shakeForm(e) {
   var thisModal = e.target;
-
   // get modal element
   while ((thisModal = thisModal.parentElement) && !thisModal.classList.contains('modal'));
   thisModal.classList.add('shake');
@@ -135,6 +103,7 @@ function toggleEditing(form, state) {
     submitButton.disabled = true;
     submitButton.innerHTML += '<div class="spinner-wrapper spinner-md"><svg viewbox="0 0 16 16" class="spinner"><circle cx="8" cy="8" r="7" stroke-linecap="round" class="path"></circle></svg></div>';
   }
+
   if (state === 'enable') {
     if (theseInputs) {
       for (i = 0; i < theseInputs.length; i++) {
@@ -163,10 +132,12 @@ function xhrSubmit(e, form, formData) {
   } else if (form.classList.contains('form-questionnaire')) {
     xhrUrl = 'https://codenow.com/submitreason';
   }
+
   // send form
   xhr.open('POST', xhrUrl);
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send(formData);
+
   xhr.onreadystatechange = function() {
     if ( xhr.readyState === 4 && xhr.status === 0) {
       shakeForm(e);
@@ -174,18 +145,22 @@ function xhrSubmit(e, form, formData) {
       toggleEditing(form, 'enable'); // re-enables form
     }
   };
+
   xhr.onload = function() {
     var response = JSON.parse(xhr.responseText);
     var resultCode = response.result_code;
     var resultMessage = response.result_message;
+
     // result_codes:
     // -1 = error from sundip
     // 0 = error from active campaign
     // 1 = success from active campaign
+
     if (resultCode === -1 || resultCode === 0) {
       shakeForm(e);
       activeCampaignValidation(resultMessage, form);
     }
+
     if (resultCode === 1) {
       if (!articleSignUp.classList.contains('out')) {
         // attach events
@@ -194,49 +169,58 @@ function xhrSubmit(e, form, formData) {
           subscriber_id: response.subscriber_id,
           email: response.email
         }));
+
         // show questionnaire
         articleSignUp.classList.add('out');
         articleQuestionnaire.classList.add('in');
+
         // segment tracking
         analytics.ready(function() {
           var data = JSON.parse(formData);
           analytics.track('Signed Up', {clientId: ga.getAll()[0].get('clientId')});
           analytics.identify(data.email, data);
         });
+
       } else {
         // else show confirmation
         articleQuestionnaire.classList.add('out');
         articleConfirm.classList.add('in');
       }
     }
+
     toggleEditing(form, 'enable'); // re-enables form
   };
 }
 
 function submitSignUp(e) {
   var form = e.target;
-
   e.preventDefault();
+
   if (form.checkValidity()) {
     var scm = document.getElementsByName('scm');
     var scmName = '';
     var formData;
+
     toggleEditing(form, 'disable'); // disables inputs
+
     // jsonify form data
     for(var i = 0; i < scm.length; i++) {
       if(scm[i].checked) {
         scmName = scm[i].value;
       }
     }
+
     formData = {
       scm: scmName,
       organization: form[2].value,
       email: form[3].value,
     };
+
     // Send event to Segment
     analytics.ready(function() {
       analytics.track('Sign Up Attempt', {scm: scmName, org: form[2].value, email: form[3].value, clientId: ga.getAll()[0].get('clientId')});
     });
+
     formData = JSON.stringify(formData); // convert to JSON
     xhrSubmit(e, form, formData);
   }
@@ -245,24 +229,26 @@ function submitSignUp(e) {
 function setupSubmitQuestionnaire(response) {
   return function (e) {
     var form = e.target;
-
     e.preventDefault();
+
     if (form.checkValidity()) {
       var formData;
 
       toggleEditing(form, 'disable'); // disables inputs
+
       formData = {
         email: response.email,
         subscriber_id: response.subscriber_id,
         reason: form[0].value
       };
+
       // Send event to Segment
       analytics.ready(function() {
         var data = {email: response.email,subscriber_id: response.subscriber_id,reason: form[0].value, clientId:ga.getAll()[0].get('clientId')};
-
         analytics.track('Questionnaire Submit', data);
         analytics.identify(data.email, data);
       });
+
       formData = JSON.stringify(formData); // convert to JSON
       xhrSubmit(e, form, formData);
     }
@@ -287,8 +273,10 @@ function activeCampaignValidation(resultMessage, form) {
       resultMessage = 'We couldn’t find that organization or team.';
       break;
   }
+
   thisErrorText.innerHTML = resultMessage;
   thisErrorWell.setAttribute('style', 'display: flex !important');
+
   // segment tracking
   analytics.ready(function() {
     analytics.track('Error submit form', {error: resultMessage, clientId: ga.getAll()[0].get('clientId')});
@@ -298,34 +286,53 @@ function activeCampaignValidation(resultMessage, form) {
 // events
 window.addEventListener('load', function(){
   var whitelisted = window.location.search !== '?whitelist=false';
-  var modalTriggers = document.getElementsByClassName('js-modal');
+  var signUpTriggers = document.getElementsByClassName('js-sign-up');
+  var signUpModal = document.getElementById('sign-up');
+  var signUpForm = document.getElementsByClassName('form-sign-up')[0];
   var questionnaireForm = document.getElementsByClassName('form-questionnaire')[0];
   var dBody = document.body;
   var dragging = false;
+  var theseInputs;
   var i;
 
   // prevent drag touch
   dBody.addEventListener('touchmove',function(){dragging = true;});
-  dBody.addEventListener('touchstart',function(){dragging = false;});
+  dBody.addEventListener('touchstart',function(){dragging = false;})
+
   // stub fbq
   if (!window.fbq) {
     window.fbq = function () {};
   }
-  // modals
-  if (modalTriggers) {
-    for (i = 0; i < modalTriggers.length; i++) {
-      /* jshint loopfunc: true */
-      modalTriggers[i].addEventListener('click', function(){openModal(dragging);});
-      modalTriggers[i].addEventListener('touchend', function(){openModal(dragging);});
+
+  // modal forms
+  if (signUpForm) {
+    signUpForm.addEventListener('change', makeDirty);
+    signUpForm.addEventListener('submit', submitSignUp);
+
+    for (i = 0; i < signUpTriggers.length; i++) {
+      signUpTriggers[i].addEventListener('click', function(){openSignUp(dragging, signUpModal)});
+      signUpTriggers[i].addEventListener('touchend', function(){openSignUp(dragging, signUpModal)});
+    }
+
+    theseInputs = signUpForm.getElementsByTagName('input');
+    for (i = 0; i < theseInputs.length; i++) {
+      theseInputs[i].addEventListener('invalid', formInvalid);
+
+      if (theseInputs[i].classList.contains('input-radio')) {
+        theseInputs[i].addEventListener('change', updateLabel);
+      }
     }
   }
+
   // show sign up if not whitelisted
   if (!whitelisted) {
     // set sign up form error
     document.getElementsByClassName('well-text')[0].innerHTML = 'You don’t have access to Runnable…yet. Fill out this form and we’ll get in touch!';
     document.getElementsByClassName('well-error')[0].setAttribute('style', 'display: flex !important');
+
     // open sign up form
     openSignUp(signUpModal);
+
     // segment tracking
     analytics.ready(function() {
       analytics.track('User not whitelisted', {clientId: ga.getAll()[0].get('clientId')});
