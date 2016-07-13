f7e226cbbe9ea2169d01134efa6570f9176cc5d4
thejsj <jorge.silva@thejsj.com>
2016-07-13 16:40:47 -0700

    Add fetching of user + whitelist

--- src/js/redirect.js
+++ src/js/redirect.js
@@ -1,26 +1,31 @@
-var app = angular.module('homeApp', []);
-
-app.controller('MainCtrl', function ($scope, $window, $http) {
-  // redirect
-  $http.get('{{apiUrl}}/users/me', {
-    withCredentials: true
-  })
-    .then(function (user) {
+(function () {
+  // Redirect
+  console.log('hello')
+  Promise.all([
+    fetch('{{apiUrl}}/users/me', { credentials: 'include' })
+      .then(function (res) { return res.json() }),
+    fetch('{{apiUrl}}/auth/whitelist', { credentials: 'include' })
+      .then(function (res) { return res.json() }),
+  ])
+    .then(function (res) {
+      var user = res[0]
+      var whitelistedOrgs = res[1]
+      if (!user || !whitelistedOrgs) {
+        return
+      }
       var org;
       try {
-        org = user.data.userOptions.uiState.previousLocation.org;
-      } catch (e) {
-        org = user.data.accounts.github.username;
-      }
+        org = user.userOptions.uiState.previousLocation.org;
+      } catch (e) {}
       var prevInstance;
       try {
-        prevInstance = user.data.userOptions.uiState.previousLocation.instance;
-      } catch (e) {
-      }
-      var newURL = '{{angularUrl}}/' + org;
+        prevInstance = user.userOptions.uiState.previousLocation.instance;
+      } catch (e) {}
+      newURL = '{{angularUrl}}/' + org;
       if (prevInstance) {
         newURL += '/' + prevInstance;
       }
-      $window.location = newURL;
+      console.log('newUrl', newUrl)
+      // $window.location = newURL;
     });
-});
+}())
