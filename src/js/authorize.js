3dc65a9bbf486b080c4d50ae4286d60a79adfc8f
runnabro <tli415@gmail.com>
2016-07-14 13:50:34 -0700

    dynamically add and remove sign in elements

--- src/js/authorize.js
+++ src/js/authorize.js
@@ -1,38 +1,27 @@
 (function () {
   // Redirect
-  console.log('hello')
   Promise.all([
     fetch('{{apiUrl}}/users/me', { credentials: 'include' })
-      .then(function (res) { return res.json() }),
+      .then(function (res) { return res.json(); }),
     fetch('{{apiUrl}}/auth/whitelist', { credentials: 'include' })
-      .then(function (res) { return res.json() }),
+      .then(function (res) { return res.json(); }),
   ])
     .then(function (res) {
-      var user = res[0]
-      var whitelistedOrgs = res[1]
+      var user = res[0];
+      var whitelistedOrgs = res[1];
 
       // Get variables we need
       var org;
       try {
         org = user.userOptions.uiState.previousLocation.org;
       } catch (e) {
-        return
+        return;
       }
       var prevInstance;
       try {
         prevInstance = user.userOptions.uiState.previousLocation.instance;
       } catch (e) {
-        return
-      }
-
-      // Show/Hide all elements
-      var els = document.getElementsByClassName('js-if-not-signed-in')
-      for (var i = 0; i < els.length; i++) {
-        els[i].style.setProperty('display', 'none', 'important')
-      }
-      var els = document.getElementsByClassName('js-if-signed-in')
-      for (var i = 0; i < els.length; i++) {
-        els[i].style.setProperty('display', 'flex', 'important')
+        return;
       }
 
       // Populate sign-in url
@@ -40,10 +29,38 @@
       if (prevInstance) {
         newURL += '/' + prevInstance;
       }
-      var els = document.getElementsByClassName('btn-sandbox')
+      var els = document.getElementsByClassName('btn-sandbox');
       for (var i = 0; i < els.length; i++) {
         els[i].href = newURL;
       }
-      console.log('newUrl', newURL)
+      console.log('newUrl', newURL);
+
+      // set up btn-sandbox in headers
+      var headerBtnSandbox = document.createElement('a');
+      var overflowMenu = document.getElementsByClassName('btn-overflow')[0];
+      headerBtnSandbox.href = newURL;
+      headerBtnSandbox.classList.add('grid-content','btn','btn-sm','btn-white','text-white','strong','btn-sandbox');
+      headerBtnSandbox.textContent = 'Your Sandbox';
+      document.getElementsByTagName('nav')[0].insertBefore(headerBtnSandbox,overflowMenu);
+
+      // set up btn-sandbox in menus
+      var menuBtnSandbox = document.createElement('a');
+      var menuDivider = document.getElementsByClassName('divider')[0];
+      menuBtnSandbox.href = newURL;
+      menuBtnSandbox.classList.add('btn','grid-block','align-center','shrink','list-item-a','btn-sandbox','strong','visible-xs');
+      menuBtnSandbox.textContent = 'Your Sandbox';
+      menuDivider.classList.add('visible-xs');
+      menuDivider.classList.remove('visible-md');
+      document.getElementsByClassName('list text-left')[0].insertBefore(menuBtnSandbox,menuDivider);
+
+      // hide sign in and sign up links
+      var signUpButtons = document.querySelectorAll('[data-target="#new-sign-up"]');
+      var signInButtons = document.querySelectorAll('[data-target="#sign-in"]');
+      for (var i = 0; i < signUpButtons.length; i++) {
+        signUpButtons[i].parentNode.removeChild(signUpButtons[i]);
+      }
+      for (var i = 0; i < signInButtons.length; i++) {
+        signInButtons[i].parentNode.removeChild(signInButtons[i]);
+      }
     });
-}())
+}());
